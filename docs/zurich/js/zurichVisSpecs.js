8b5fb85a4f96b7e516f876e5fc66a957014236da
Jo Wood <jwo@soi.city.ac.uk>
2020-04-30 18:38:49 +0100

    Add padding to left of vis specs

--- docs/zurich/js/zurichVisSpecs.js
+++ docs/zurich/js/zurichVisSpecs.js
@@ -38,7 +38,12 @@ let vlSpecLinkedBicycle = {
     },
   },
   spacing: 0,
-  center: false,
+  padding: {
+    left: 10,
+    top: 0,
+    right: 0,
+    bottom: 0,
+  },
   concat: [
     {
       columns: 1,
