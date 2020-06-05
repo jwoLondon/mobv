65edeeedae4abb3e057dff3388af2f5d1c6656e7
rspurves <ross.purves@geo.uzh.ch>
2020-06-05 13:48:54 +0200

    Scaling for very high peaks.

--- docs/newyork/js/newYorkVisSpecs.js
+++ docs/newyork/js/newYorkVisSpecs.js
@@ -15,8 +15,8 @@ let greenPolyFeature = "parks";
 
 // CHART SCALING
 
-let bicycleAnomalyMax = 150;
-let bicycleAnomalyMin = -150;
+let bicycleAnomalyMax = 200;
+let bicycleAnomalyMin = -80;
 
 // -----------------------------------------------------------------------------
 
