e9080fb22609842adb922dc61e72058fe1941136
Ross Purves <ross.purves@geo.uzh.ch>
2020-07-15 09:08:09 +0200

    Testing Paris

---
+++ docs/paris/index.md
@@ -0,0 +1,41 @@
+<!-- Scripts to link to the Vega/Vega-Lite runtime -->
+<script src="https://cdn.jsdelivr.net/npm/vega@5"></script>
+<script src="https://cdn.jsdelivr.net/npm/vega-lite@4.5.1"></script>
+<script src="https://cdn.jsdelivr.net/npm/vega-embed@6"></script>
+
+# Bewegung in der Corona-Zeit
+
+Wie können wir Bewegung verstehen und vergleichen? Bewegen wir uns in der aktuellen Situation mehr oder weniger als üblich? Und wo sind wir unterwegs? Die folgenden Visualisierungen – Heatmaps, Liniengrafiken und Karten – sollen Entscheidungsträgerinnen und Entscheidungsträgern eine Übersicht geben und es ihnen erlauben, auf einfache Art Vergleiche zwischen Standorten in der Stadt Zürich und über die Zeit anzustellen.
+
+## Visualisierungen
+
+Die Visualisierungen sind konzipiert für unterschiedliche Fortbewegungsarten. Bisher umgesetzt sind Analysen für den Velo- und den Fussverkehr in der Stadt Zürich. Statt absoluten Zahlen zeigen wir das Verhältnis aktueller Zahlen zu einem für jeden Standort einzeln berechneten Referenzwert. Der Referenzwert entspricht dem mittleren (Median) täglichen Verkehr zwischen dem 3. Januar und dem 6. Februar 2020 und wird pro Wochentag einzeln berechnet. In den Visualisierungen ist das Verhältnis aktueller Tageswerte zu diesen Referenzwerten dargestellt: in blauen Kästchen in der sogenannten Heatmap fällt das tägliche Fuss- bzw. Veloverkehrsaufkommen tiefer aus als anhand der Referenz erwartet, in orangen höher. Die Zählstellen sind in der Visualisierung anhand ihres Verkehrsaufkommens sortiert: Zählstellen mit viel Verkehr sind zuoberst, Zählstellen mit tieferen Verkehrszahlen weiter unten.
+
+Die Heatmaps sind dynamisch: Durch Überfahren (Hovern) der Balken mit der Maus werden das Verkehrsaufkommen am betreffenden Tag, der Referenzwert (Median) und die Anomalie (der sogenannte Chi-Wert) angezeigt. Mit einem Mausklick kann man eine Zählstelle selektieren, durch Betätigen der Shift-Taste und Mausklick mehrere gleichzeitig. Die selektierten Zählstellen werden in den begleitenden Visualisierungen – der Liniengrafik und der Karte – hervorgehoben. So können Sie schnell und einfach unterschiedliche Standorte miteinander verglichen.
+
+Die vertikalen Muster in der Heatmap und der Liniengrafik zeigen deutlich, wie die Mobilität in Zürich sich nach dem Lockdown Mitte März rasch reduziert – wobei sich aber nicht alle Standorte gleich verhalten. Es sind auch andere Muster sichtbar: wie der Veloverkehr in der Regel am Wochenende schnell wieder zunimmt oder wie das regnerische Wetter ab 28. April zu einem generellen und starken Rückgang des Verkehrs führt.
+
+## Datenquellen und Umsetzung
+
+Wir hätten unsere Visualisierungen nicht umsetzen können ohne [Open Government Data (OGD) der Stadt Zürich](https://data.stadt-zuerich.ch/dataset/ted_taz_verkehrszaehlungen_werte_fussgaenger_velo). Ein grosses Dankeschön geht an die Abteilung Verkehr + Stadtraum im [Tiefbauamt der Stadt Zürich](https://www.stadt-zuerich.ch/ted/de/index/taz.html) für die Erhebung der Mobilitätsdaten und die Bereitstellung als offene Verwaltungsdaten zusammen mit dem [Open Data Zürich-Team](https://www.stadt-zuerich.ch/portal/de/index/ogd.html) bei [Statistik Stadt Zürich](https://www.stadt-zuerich.ch/prd/de/index/statistik.html) sowie an die [OpenStreetMap-Beitragenden](https://www.openstreetmap.org/copyright) für die Kartendaten.
+
+Analoge Visualisierungen haben wir mit [Open Government Data des Kantons Basel Stadt](https://jwolondon.github.io/mobv/docs/zurich/) und der [Stadt New York](https://jwolondon.github.io/mobv) umgesetzt. Sämtlicher Code für die Analyse der Daten von Zürich, Basel und New York und der Code hinter den jeweiligen Webseiten ist verfügbar auf [GitHub](https://github.com/jwolondon/mobv/). Sämtlicher Code steht [unter einer offenen Lizenz](https://github.com/jwoLondon/mobv/blob/master/LICENSE) zur Weiterverwendung zur Verfügung.
+
+## Team
+
+Konzeptiert und implementiert von:
+
+- _[Ross Purves](https://twitter.com/GCUZH) – [Department of Geography, University of Zurich](https://www.geo.uzh.ch/~rsp/)_
+- _[Ralph Straumann](https://twitter.com/rastrau) – [EBP, Data Science Team, Zurich](https://www.ebp.ch)_
+- _[Jo Wood](https://twitter.com/jwolondon) – [giCentre, City, University of London](https://www.gicentre.net/jwo)_
+
+# Veloverkehr
+
+<div class="wide" id="visLinkedBicycle"></div>
+
+# Fussverkehr
+
+<div class="wide" id="visLinkedFoot"></div>
+
+<!-- Script containing the vis specs used above. Must be at end of document. -->
+<script src="js/zurichVisSpecs.js"></script>
---
+++ docs/paris/js/parisVisSpecs.js
@@ -0,0 +1,488 @@
+// DATA SOURCES
+
+let path = "https://jwolondon.github.io/mobv/data/paris/";
+
+let bicycleStationData = path + "StationLocations-Bicycle.csv";
+let bicycleTimeSeriesData = path + "StationDailyTimeSeries-Bicycle.csv";
+let bicycleReferenceData = path + "StationReference-Bicycle.csv";
+
+let footStationData = path + "StationLocations-Foot.csv";
+let footTimeSeriesData = path + "StationDailyTimeSeries-Foot.csv";
+let footReferenceData = path + "StationReference-Foot.csv";
+
+// let annotationsData = path + "annotations.csv";
+
+// let blueLines = path + "geo/zurichRivers.json";
+// let blueLineFeature = "zurichLines";
+// let bluePolys = path + "geo/zurichLakes.json";
+// let bluePolyFeature = "zurichPolys";
+// let greenPolys = path + "geo/zurichGreen.json";
+// let greenPolyFeature = "zurichPolys";
+
+// CHART SCALING
+
+let bicycleAnomalyMax = 120;
+let bicycleAnomalyMin = -120;
+let footAnomalyMax = 140;
+let footAnomalyMin = -140;
+
+// -----------------------------------------------------------------------------
+
+// BICYCLE LINKED VIEWS CHART
+
+let vlSpecLinkedBicycle = {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  config: {
+    view: {
+      stroke: "",
+    },
+  },
+  spacing: 0,
+  padding: {
+    left: 10,
+    top: 0,
+    right: 0,
+    bottom: 0,
+  },
+  data: { url: `${bicycleTimeSeriesData}` },
+  transform: [
+    {
+      lookup: "id",
+      from: {
+        data: { url: `${bicycleReferenceData}` },
+        key: "id",
+        fields: ["value"],
+      },
+    },
+    {
+      calculate:
+        "datum.value == 0 ? 0 : (datum.count - datum.value)/sqrt(datum.value)",
+      as: "anomaly",
+    },
+    {
+      lookup: "station",
+      from: {
+        data: { url: `${bicycleStationData}` },
+        key: "station_id",
+        fields: ["station_name"],
+      },
+    },
+    {
+      filter: "datum.station_name != null",
+    },
+  ],
+  hconcat: [
+    {
+      vconcat: [
+        {
+          width: 800,
+          height: {
+            step: 12,
+          },
+          layer: [
+            {
+              selection: {
+                brush: {
+                  type: "multi",
+                  fields: ["station_name"],
+                },
+              },
+              encoding: {
+                x: {
+                  field: "date",
+                  type: "temporal",
+                  axis: {
+                    gridOpacity: {
+                      condition: {
+                        test: "day(datum.value) == 6 || day(datum.value) == 0",
+                        value: 1,
+                      },
+                      value: 0,
+                    },
+                    tickSize: {
+                      condition: {
+                        test: "day(datum.value) == 1",
+                        value: 6,
+                      },
+                      value: 3,
+                    },
+                    tickCount: 200,
+                    gridWidth: 8,
+                    gridColor: "#f6f6f6",
+                    labelExpr:
+                      "timeFormat(datum.value, '%a') == 'Mon' ? timeFormat(datum.value, '%e %b') : ''",
+                    title: "",
+                    labelFont: "Roboto Condensed",
+                  },
+                },
+                y: {
+                  field: "station_name",
+                  type: "nominal",
+                  sort: {
+                    field: "count",
+                    op: "mean",
+                    order: "descending",
+                  },
+                  axis: {
+                    title: "",
+                    offset: 7,
+                    domain: false,
+                    ticks: false,
+                    labelFont: "Roboto Condensed",
+                  },
+                },
+                color: {
+                  field: "anomaly",
+                  type: "quantitative",
+                  scale: {
+                    scheme: "blueOrange",
+                    domainMid: 0,
+                  },
+                  legend: {
+                    title: "Anomaly",
+                    direction: "horizontal",
+                    orient: "top",
+                    gradientThickness: 8,
+                  },
+                },
+                opacity: {
+                  condition: {
+                    selection: "brush",
+                    value: 1,
+                  },
+                  value: 0.5,
+                },
+                size: {
+                  condition: {
+                    selection: "brush",
+                    value: 90,
+                  },
+                  value: 40,
+                },
+                tooltip: [
+                  {
+                    field: "station_name",
+                    type: "nominal",
+                    title: "locality",
+                  },
+                  {
+                    field: "date",
+                    type: "temporal",
+                    format: "%a %e %b",
+                  },
+                  {
+                    field: "value",
+                    type: "quantitative",
+                    title: "expected",
+                    format: ".0f",
+                  },
+                  {
+                    field: "count",
+                    type: "quantitative",
+                    title: "observed",
+                    format: ".0f",
+                  },
+                  {
+                    field: "anomaly",
+                    type: "quantitative",
+                    format: ".1f",
+                  },
+                ],
+              },
+              mark: "square",
+            },
+            {
+              data: { url: `${annotationsData}` },
+              layer: [
+                {
+                  encoding: {
+                    x: {
+                      field: "date",
+                      type: "temporal",
+                    },
+                  },
+                  mark: {
+                    type: "rule",
+                    strokeDash: [4, 2],
+                    opacity: 0.5,
+                  },
+                },
+                {
+                  encoding: {
+                    x: {
+                      field: "date",
+                      type: "temporal",
+                    },
+                    y: {
+                      value: 0,
+                    },
+                    text: {
+                      field: "notes",
+                      type: "nominal",
+                    },
+                  },
+                  mark: {
+                    type: "text",
+                    angle: 310,
+                    align: "left",
+                    opacity: 0.5,
+                    fontSize: 8,
+                    font: "Roboto Condensed",
+                    dx: 2,
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        {
+          width: 800,
+          height: 400,
+          layer: [
+            {
+              selection: {
+                brush: {
+                  type: "multi",
+                  fields: ["station_name"],
+                },
+              },
+              encoding: {
+                x: {
+                  field: "date",
+                  type: "temporal",
+                  axis: {
+                    gridOpacity: {
+                      condition: {
+                        test: "day(datum.value) == 6 || day(datum.value) == 0",
+                        value: 1,
+                      },
+                      value: 0,
+                    },
+                    tickSize: {
+                      condition: {
+                        test: "day(datum.value) == 1",
+                        value: 6,
+                      },
+                      value: 3,
+                    },
+                    tickCount: 200,
+                    gridWidth: 8,
+                    gridColor: "#f6f6f6",
+                    labelExpr:
+                      "timeFormat(datum.value, '%a') == 'Mon' ? timeFormat(datum.value, '%e %b') : ''",
+                    title: "",
+                    labelFont: "Roboto Condensed",
+                  },
+                },
+                y: {
+                  field: "anomaly",
+                  type: "quantitative",
+                  scale: {
+                    domain: [`${bicycleAnomalyMin}`, `${bicycleAnomalyMax}`],
+                    nice: false,
+                  },
+                  title: "Anomaly",
+                  labelFont: "Roboto Condensed",
+                },
+                color: {
+                  condition: {
+                    selection: "brush",
+                    field: "station_name",
+                    type: "nominal",
+                    legend: null,
+                  },
+                  value: "black",
+                },
+                opacity: {
+                  condition: {
+                    selection: "brush",
+                    value: 1,
+                  },
+                  value: 0.1,
+                },
+                size: {
+                  condition: {
+                    selection: "brush",
+                    value: 2,
+                  },
+                  value: 0.2,
+                },
+              },
+              mark: {
+                type: "line",
+                interpolate: "monotone",
+                clip: true,
+              },
+            },
+            {
+              data: {
+                values: [
+                  {
+                    origin: 0,
+                  },
+                ],
+              },
+              encoding: {
+                y: {
+                  field: "origin",
+                  type: "quantitative",
+                },
+              },
+              mark: "rule",
+            },
+            {
+              data: { url: `${annotationsData}` },
+              encoding: {
+                x: {
+                  field: "date",
+                  type: "temporal",
+                },
+              },
+              mark: {
+                type: "rule",
+                strokeDash: [4, 2],
+                opacity: 0.5,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      width: 400,
+      height: 650,
+      transform: [
+        {
+          aggregate: [
+            {
+              op: "count",
+              field: "station_name",
+              as: "numReadings",
+            },
+          ],
+          groupby: ["station_name"],
+        },
+        {
+          lookup: "station_name",
+          from: {
+            data: { url: `${bicycleStationData}` },
+            key: "station_name",
+            fields: ["lon", "lat"],
+          },
+        },
+      ],
+      /* layer: [
+        {
+          layer: [
+            {
+              data: {
+                url: `${greenPolys}`,
+                format: {
+                  type: "topojson",
+                  feature: `${greenPolyFeature}`,
+                },
+              },
+              mark: {
+                type: "geoshape",
+                color: "rgb(239,244,225)",
+              },
+            },
+            {
+              data: {
+                url: `${blueLines}`,
+                format: { type: "topojson", feature: `${blueLineFeature}` },
+              },
+
+              mark: {
+                type: "geoshape",
+                color: "rgb(226,237,246)",
+                strokeWidth: 4,
+                filled: false,
+              },
+            },
+            {
+              data: {
+                url: `${bluePolys}`,
+                format: { type: "topojson", feature: `${bluePolyFeature}` },
+              },
+              mark: {
+                type: "geoshape",
+                color: "rgb(226,237,246)",
+              },
+            },
+          ],
+        }, */
+        {
+          selection: {
+            brush: {
+              type: "multi",
+              fields: ["station_name"],
+            },
+          },
+          encoding: {
+            longitude: {
+              field: "lon",
+              type: "quantitative",
+            },
+            latitude: {
+              field: "lat",
+              type: "quantitative",
+            },
+            color: {
+              field: "station_name",
+              type: "nominal",
+              legend: null,
+            },
+            opacity: {
+              condition: {
+                selection: "brush",
+                value: 1,
+              },
+              value: 0.1,
+            },
+          },
+          mark: {
+            type: "square",
+            size: 80,
+          },
+        },
+        {
+          encoding: {
+            longitude: {
+              field: "lon",
+              type: "quantitative",
+            },
+            latitude: {
+              field: "lat",
+              type: "quantitative",
+            },
+            text: {
+              field: "station_name",
+              type: "nominal",
+            },
+            opacity: {
+              condition: {
+                selection: "brush",
+                value: 1,
+              },
+              value: 0.1,
+            },
+          },
+          mark: {
+            type: "text",
+            dy: 10,
+            fontSize: 6,
+            font: "Roboto Condensed",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+
+// -----------------------------------------------------------------------------
+// Reference each of the specs with an ID that can be used in the main HTML.
+// If a new spec is added above, add its name along with a corresponding DOM id.
+
+vegaEmbed("#visLinkedBicycle", vlSpecLinkedBicycle).catch(console.error);
